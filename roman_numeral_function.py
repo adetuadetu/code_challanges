46c0e4e5bd9bd9a57546a62fc4ec9831eb4c9634
adetuadetu <jposorio95@hotmail.com>
2023-03-24 13:28:20 +0100

    addition of notes for clarity

--- roman_numeral_function.py
+++ roman_numeral_function.py
@@ -1,19 +1,42 @@
+# Initialisation of function with one parameter which will be the roman numeral
 def RomanToNum(s):
     
+    # Key with all primary symbol values of roman numerals
     roman = {"M":1000, "CM":900, "D":500, "CD":400, "C":100, "XC":90, "L":50, "XL":40, "X":10, "IX":9, "V":5, "IV":4, "I":1}
 
-    i = 0 
+    # Iteration value
+    i = 0
+
+    # Converted tally of roman numeral
     num = 0
 
+    # Iteration through length of parameter 
     while i < len(s):
+
+        # Iteration through each roman numeral whilst checking if the numeral next to it and itself is within the key
         if i + 1 < len(s) and s[i:i+2] in roman:
+
+            # If the double numeral is within the key the value will be added to the tally 
+            # eg if the double numeral is IX; I = first i X = second i and +2 will be added to the iteration
             num += roman[s[i:i+2]]
+
+            # Iteration will be +2 since the double numeral equates to a single value 
             i += 2
+
+        # If not a double numeral value 
         else:
+            
+            # Add value of single numeral to tally
             num += roman[s[i]]
+
+            # Iterate to next number
             i += 1
+
+    # Return the tallied result 
     return num
 
-solution = RomanToNum("MMXXIII")
+# Converted result using the function will be saved in this variable
+solution = RomanToNum("MMXIXIII")
+
+# Print result 
 print(solution)
-print("All done")
